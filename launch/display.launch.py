cc7a64a1778ecff46b27bab44f83aaa866eeb9a6
shantanuparabumd <sparab@umd.edu>
2023-08-13 11:36:26 -0400

    Display Launch File Ready

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -7,11 +7,40 @@ import os
 from launch.substitutions import Command
 import launch_ros.descriptions
 import launch
+import xacro
 
 def generate_launch_description():
-    package_description = "project_legion"
+    
+    ####### DATA INPUT ##########
+    urdf_file = 'robotaxi.urdf'
     xacro_file = "robotaxi.urdf.xacro"
-    entity_name="taxi"
+    #xacro_file = "box_bot.xacro"
+    package_description = "project_legion"
+    use_urdf = False
+    # Position and orientation
+    # [X, Y, Z]
+    position = [0.0, 0.0, 0.5]
+    # [Roll, Pitch, Yaw]
+    orientation = [0.0, 0.0, 0.0]
+    # Base Name or robot
+    robot_base_name = "robotaxi"
+    ####### DATA INPUT END ##########
+    pkg_share = launch_ros.substitutions.FindPackageShare(package=package_description).find(package_description)
+
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz/robotaxi.rviz')
+
+
+    if use_urdf:
+        # print("URDF URDF URDF URDF URDF URDF URDF URDF URDF URDF URDF ==>")
+        robot_desc_path = os.path.join(get_package_share_directory(
+            package_description), "robot", urdf_file)
+    else:
+        # print("XACRO XACRO XACRO XACRO XACRO XACRO XACRO XACRO XACRO XACRO XACRO ==>")
+        robot_desc_path = os.path.join(get_package_share_directory(
+            package_description), "urdf", xacro_file)
+
+    robot_desc = xacro.process_file(robot_desc_path)
+    xml = robot_desc.toxml()
 
 
  
@@ -31,15 +60,33 @@ def generate_launch_description():
         parameters=[{'use_sim_time': True}],
         arguments=['-d', rviz_config_dir])
     
-    robot_state_publisher_node = Node(
+    # Publish Robot Desciption in String form in the topic /robot_description
+    publish_robot_description = Node(
+        package='project_legion',
+        executable='robot_description_publisher.py',
+        name='robot_description_publisher',
+        output='screen',
+        arguments=['-xml_string', xml,
+                   '-robot_description_topic', '/robot_description'
+                   ]
+    )
+
+    # Robot State Publisher
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    robot_state_publisher= Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher',
-        namespace=entity_name,
-        parameters=[{'robot_description': launch_ros.descriptions.ParameterValue(launch.substitutions.Command(['xacro ',robot_desc_path]), value_type=str)  }],
+        parameters=[{'use_sim_time': use_sim_time, 'robot_description': xml}],
         output="screen"
     )
 
+    joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+    )
     joint_state_gui=Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
@@ -48,7 +95,15 @@ def generate_launch_description():
         )
     
     return LaunchDescription([
-        robot_state_publisher_node,
+        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
+                                            description='Flag to enable joint_state_publisher_gui'),
+        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                                            description='Absolute path to rviz config file'),
+        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                        description='Flag to enable use_sim_time'),
+        publish_robot_description,
+        joint_state_publisher_node,
+        robot_state_publisher,
         rviz_node,
         joint_state_gui,
         
